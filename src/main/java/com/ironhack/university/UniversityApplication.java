ce96089ff043cb512d747d911b717d5165e052ca
jaume-ironhack <jaume.sanchez@ironhack.com>
2022-08-16 11:26:17 +0200

    Intro to spring boot

---
+++ src/main/java/com/ironhack/university/UniversityApplication.java
@@ -0,0 +1,14 @@
+package com.ironhack.university;
+
+import com.ironhack.university.model.Course;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityApplication {
+
+	public static void main(String[] args) {
+ 		SpringApplication.run(UniversityApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ironhack/university/model/Course.java
@@ -0,0 +1,39 @@
+package com.ironhack.university.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    private String courseCode;
+    @Column(name = "nombre_del_curso")
+    private String courseName;
+
+    public Course(){};
+
+    public Course(String courseCode, String courseName) {
+        this.courseCode = courseCode;
+        this.courseName = courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+}
