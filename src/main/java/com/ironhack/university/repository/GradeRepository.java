4f8f9d88d576e76dcafd291dbb15061a1620e7dc
jaume-ironhack <jaume.sanchez@ironhack.com>
2022-08-17 11:00:00 +0200

    Aded JPA queries

--- src/main/java/com/ironhack/university/repository/GradeRepository.java
+++ src/main/java/com/ironhack/university/repository/GradeRepository.java
@@ -4,6 +4,16 @@ import com.ironhack.university.model.Grade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface GradeRepository extends JpaRepository<Grade, Long> {
+
+    //SELECT * FROM grade WHERE student_name = :studentName and section_id = :section_id;
+    Optional<Grade> findByStudentNameAndSectionId(String studentName, String sectionId);
+    Grade findByStudentName(String studentName);
+    List<Grade> findAllByStudentNameContaining(String studentName);
+    List<Grade> findByScoreBetween(Integer min, Integer max);
+    List<Grade> findByScoreGreaterThan(Integer min);
 }
--- src/test/java/com/ironhack/university/repository/GradeRepositoryTest.java
+++ src/test/java/com/ironhack/university/repository/GradeRepositoryTest.java
@@ -8,13 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class GradeRepositoryTest {
     private Grade grade1;
     private Grade grade2;
+    private Grade grade3;
+    private Grade grade4;
+    private Grade grade5;
 
     @Autowired
     GradeRepository gradeRepository;
@@ -23,8 +27,10 @@ public class GradeRepositoryTest {
     void setUp() {
         grade1 = new Grade("Aldo Santa Maria", "CS101-A", 75);
         grade2 = new Grade("Raúl Galán", "CS101-A", 80);
-        gradeRepository.save(grade1);
-        gradeRepository.save(grade2);
+        grade3 = new Grade("Gerard", "CS101-A", 60);
+        grade4 = new Grade("Alessio", "CS101-A", 90);
+        grade5 = new Grade("Nil", "CS101-A", 75);
+        gradeRepository.saveAll(List.of(grade1, grade2, grade3, grade4, grade5));
     }
 
     @AfterEach
@@ -53,5 +59,63 @@ public class GradeRepositoryTest {
         List<Grade> result  = gradeRepository.findAll();
 
         assertEquals(1, result.size());
+    }
+
+    /*
+        Optional<Grade> findByStudentNameAndSectionId(String studentName, String sectionId);
+    List<Grade> findAllByStudentNameContaining(String studentName);
+    List<Grade> findByScoreBetween(Integer min, Integer max);
+    List<Grade> findByScoreGreaterThan(Integer min);
+     */
+
+    @Test
+    void findByStudentNameAndSectionId() {
+        Optional<Grade> expected = gradeRepository
+                .findByStudentNameAndSectionId("Aldo Santa Maria","CS101-A");
+
+        assertTrue(expected.isPresent());
+        assertEquals(grade1.getStudentName(), expected.get().getStudentName());
+
+        expected = gradeRepository
+                // Checking that the method returns an empty Optional when the student name and section id are not found.
+                .findByStudentNameAndSectionId("Aldo Santa Maria","CS101-B");
+        assertFalse(expected.isPresent());
+
+        expected = gradeRepository
+                .findByStudentNameAndSectionId("Lado Santa Maria","CS101-A");
+
+        assertFalse(expected.isPresent());
+
+    }
+
+    @Test
+    void findByStudentName() {
+        Grade grade = gradeRepository.findByStudentName("Aldo Santa Maria");
+
+        assertEquals("Aldo Santa Maria", grade.getStudentName());
+
+        grade = gradeRepository.findByStudentName("Pepe");
+
+        assertEquals(null, grade);
+
+    }
+
+    @Test
+    void findScoreBetween() {
+        List<Grade> grades = gradeRepository.findByScoreBetween(50, 76);
+
+        for (Grade grade : grades) {
+            System.out.println(grade.getStudentName());
+        }
+
+        assertEquals(3, grades.size());
+
+        grades = gradeRepository.findByScoreBetween(89, 91);
+        assertEquals(1, grades.size());
+
+        grades = gradeRepository.findByScoreGreaterThan(95);
+        assertEquals(0, grades.size());
+
+
     }
 }
